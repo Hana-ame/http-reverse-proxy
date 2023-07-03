24e079864eea6e5e54afd386f8c6f5d88a2f91dd
lumino <luminovoez@gmail.com>
2023-07-03 14:32:07 +0800

    add -dual-stack flag

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ export GOARCH=amd64
 export CGO_ENABLED=0
 # export CC=aarch64-linux-gnu-gcc
 
-go build -o a.bin
+go build -o a.out
--- main.go
+++ main.go
@@ -4,18 +4,20 @@ import (
 	"flag"
 	"io"
 	"log"
+	"net"
 	"net/http"
+	"time"
 )
 
-var Client *http.Client = &http.Client{}
+var Client *http.Client
 
-func httpHandler(trueHost string) func(w http.ResponseWriter, r *http.Request) {
+func httpHandler(trueHost, cookie string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		newUrl := r.URL
 		newUrl.Host = trueHost
 		newUrl.Scheme = "https"
 
-		req, err := http.NewRequest("GET", newUrl.String(), nil)
+		req, err := http.NewRequest("GET", newUrl.String(), r.Body)
 		if err != nil {
 			log.Println(err.Error())
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -24,10 +26,10 @@ func httpHandler(trueHost string) func(w http.ResponseWriter, r *http.Request) {
 		for k, v := range r.Header {
 			req.Header.Set(k, v[0])
 		}
-		if COOKIE != "" {
-			req.Header.Set("Cookie", COOKIE)
+		if cookie != "" {
+			req.Header.Set("Cookie", cookie)
 		}
-		req.Header.Set("Referer", HOST)
+		req.Header.Set("Referer", trueHost)
 
 		resp, err := Client.Do(req)
 		if err != nil {
@@ -54,14 +56,35 @@ func httpHandler(trueHost string) func(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func InitClient(dualStack bool) {
+	log.Printf("[log]init client with dualStack:%v\n", dualStack)
+	tr := &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout:   5 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: dualStack, // Forces IPv4 only when set to `false`
+		}).Dial,
+	}
+
+	client := &http.Client{
+		Transport: tr,
+	}
+
+	Client = client
+}
+
 func main() {
 	var laddr = flag.String("l", "127.0.0.1:8080", "listen address")
-	var saddr = flag.String("s", "s.exhentai.org", "server address")
+	var saddr = flag.String("s", HOST, "server address")
+	var cookie = flag.String("cookie", COOKIE, "cookie")
+	var dualStack = flag.Bool("-dual-stack", false, "Forces IPv4 only when not set this flag")
 	// var reExp = flag.String("r", ".*", "regex to match")
 	flag.Parse()
 
+	InitClient(*dualStack)
+
 	handler := http.NewServeMux()
-	handler.HandleFunc("/", httpHandler(*saddr))
+	handler.HandleFunc("/", httpHandler(*saddr, *cookie))
 	server := &http.Server{Addr: *laddr, Handler: handler}
 
 	err := server.ListenAndServe()
